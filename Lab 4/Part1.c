527a5f71401b834be3b97fdfb2c5d1cfe94eb370
Shreetej Reddy <reddyshr@sas.upenn.edu>
2017-02-23 19:47:40 -0500

    Adding the initial code for part 1

---
+++ Lab 4/Part1.c
@@ -0,0 +1,24 @@
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+unsigned int half_period = 18181;
+
+ISR (TIMER2_COMPA_vect) {
+	OCR1A += half_period;
+}
+
+int main(void)
+{
+	DDRB |= (1 << PB1);
+	TCCR1A |= 0x40;
+	TCCR1B |= 0x01;
+	TIMSK1 = 0x02;		
+	OCR1A += TCNT1 + half_period;
+	sei();
+	
+    while(1)
+    {        
+    }
+}
