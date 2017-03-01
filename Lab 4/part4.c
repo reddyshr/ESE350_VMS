40f3d7b78804c2878b11490e994e044e3058faa7
Victor Chek <VictorChek@hntvlan531.00480.wlan.sas.upenn.edu>
2017-03-01 14:50:55 -0500

    deleted functions that calculated delays, replaced them in the decode function

--- Lab 4/part4.c
+++ Lab 4/part4.c
@@ -3,7 +3,12 @@
 #include <stdio.h>
 #include "uart.h"
 
-const int TIMER = 62500;
+const int TIMER = 62500; //prescaler is 256
+
+volatile int row_to_check;
+volatile int row_delay;
+volatile int col_delay;
+
 /*
 PC2 - pin8/row4
 PC3 - pin1/row3
@@ -16,44 +21,32 @@ PD4 - pin6/col3
 PD5 - pin7/col4
 */
 
-volatile int row_to_check;
-volatile int row_delay;
-volatile int col_delay;
-
 void decode() {
 	if (row_to_check == 4) {
 		PORTC &= ~(1 << PC2);
 		PORTC |= (1 << PC3) | (1 << PC4) | (1 << PC5);
 		if (((PIND & (1 << PD2)) >> PD2) == 0) {
-			//column 1
-			//putchar('1');
-			//PB1 = 697Hz, PB2 = 1209Hz
-			row_delay = wave_697Hz();
-			col_delay = wave_1209Hz();
+			//column 1, number 1, PB1 = 697Hz, PB2 = 1209Hz
+			row_delay = (TIMER / 697) / 2;
+			col_delay = (TIMER / 1209) / 2;
 			while(((PIND & (1 << PD2)) >> PD2) == 0);
 		}
 		else if (((PIND & (1 << PD3)) >> PD3) == 0) {
-			//column 2
-			//putchar('2');
-			//PB1 = 697Hz, PB2 = 1336Hz
-			row_delay = wave_697Hz();
-			col_delay = wave_1336Hz();
+			//column 2, number 2, PB1 = 697Hz, PB2 = 1336Hz
+			row_delay = (TIMER / 697) / 2;
+			col_delay = (TIMER / 1336) / 2;
 			while(((PIND & (1 << PD3)) >> PD3) == 0);
 		}
 		else if (((PIND & (1 << PD4)) >> PD4) == 0) {
-			//column 3
-			//putchar('3');
-			//PB1 = 697Hz, PB2 = 1477Hz
-			row_delay = wave_697Hz();
-			col_delay = wave_1477Hz();
+			//column 3, number 3, PB1 = 697Hz, PB2 = 1477Hz
+			row_delay = (TIMER / 697) / 2;
+			col_delay = (TIMER / 1477) / 2;
 			while(((PIND & (1 << PD4)) >> PD4) == 0);
 		}
 		else if (((PIND & (1 << PD5)) >> PD5) == 0) {
-			//column 4
-			//putchar('A');
-			//PB1 = 697Hz, PB2 = 1633Hz
-			row_delay = wave_697Hz();
-			col_delay = wave_1633Hz();
+			//column 4, letter A, PB1 = 697Hz, PB2 = 1633Hz
+			row_delay = (TIMER / 697) / 2;
+			col_delay = (TIMER / 1633) / 2;
 			while(((PIND & (1 << PD5)) >> PD5) == 0);
 		}
 		else {
@@ -64,35 +57,27 @@ void decode() {
 		PORTC &= ~(1 << PC3);
 		PORTC |= (1 << PC2) | (1 << PC4) | (1 << PC5);
 		if (((PIND & (1 << PD2)) >> PD2) == 0) {
-			//column 1;
-			//putchar('4');
-			//PB1 = 770Hz, PB2 = 1209Hz
-			row_delay = wave_770Hz();
-			col_delay = wave_1209Hz();
+			//column 1, number 4, PB1 = 770Hz, PB2 = 1209Hz
+			row_delay = (TIMER / 770) / 2;
+			col_delay = (TIMER / 1209) / 2;
 			while(((PIND & (1 << PD2)) >> PD2) == 0);
 		}
 		else if (((PIND & (1 << PD3)) >> PD3) == 0) {
-			//column 2
-			//putchar('5');
-			//PB1 = 770Hz, PB2 = 1336Hz
-			row_delay = wave_770Hz();
-			col_delay = wave_1336Hz();
+			//column 2, number 5, PB1 = 770Hz, PB2 = 1336Hz
+			row_delay = (TIMER / 770) / 2;
+			col_delay = (TIMER / 1336) / 2;
 			while(((PIND & (1 << PD3)) >> PD3) == 0);
 		}
 		else if (((PIND & (1 << PD4)) >> PD4) == 0) {
-			//column 3
-			//putchar('6');
-			//PB1 = 770Hz, PB2 = 1477Hz
-			row_delay = wave_770Hz();
-			col_delay = wave_1477Hz();
+			//column 3, number 6, PB1 = 770Hz, PB2 = 1477Hz
+			row_delay = (TIMER / 770) / 2;
+			col_delay = (TIMER / 1477) / 2;
 			while(((PIND & (1 << PD4)) >> PD4) == 0);
 		}
 		else if (((PIND & (1 << PD5)) >> PD5) == 0) {
-			//column 4
-			//putchar('B');
-			//PB1 = 770Hz, PB2 = 1633Hz
-			row_delay = wave_770Hz();
-			col_delay = wave_1633hz();
+			//column 4, letter B, PB1 = 770Hz, PB2 = 1633Hz
+			row_delay = (TIMER / 770) / 2;
+			col_delay = (TIMER / 1633) / 2;
 			while(((PIND & (1 << PD5)) >> PD5) == 0);
 		}
 		else {
@@ -103,35 +88,27 @@ void decode() {
 		PORTC &= ~(1 << PC4);
 		PORTC |= (1 << PC2) | (1 << PC3) | (1 << PC5);
 		if (((PIND & (1 << PD2)) >> PD2) == 0) {
-			//column 1;
-			//putchar('7');
-			//PB1 = 852Hz, PB2 = 1209Hz
-			row_delay = wave_852Hz();
-			col_delay = wave_1209Hz();
+			//column 1, number 7, PB1 = 852Hz, PB2 = 1209Hz
+			row_delay = (TIMER / 852) / 2;
+			col_delay = (TIMER / 1209) / 2;
 			while(((PIND & (1 << PD2)) >> PD2) == 0);
 		}
 		else if (((PIND & (1 << PD3)) >> PD3) == 0) {
-			//column 2
-			//putchar('8');
-			//PB1 = 852Hz, PB2 = 1336Hz
-			row_delay = wave_852Hz();
-			col_delay = wave_1336Hz();
+			//column 2, number 8, PB1 = 852Hz, PB2 = 1336Hz
+			row_delay = (TIMER / 852) / 2;
+			col_delay = (TIMER / 1336) / 2;
 			while(((PIND & (1 << PD3)) >> PD3) == 0);
 		}
 		else if (((PIND & (1 << PD4)) >> PD4) == 0) {
-			//column 3
-			//putchar('9');
-			//PB1 = 852Hz, PB2 = 1477Hz
-			row_delay = wave_852Hz();
-			col_delay = wave_1477Hz();
+			//column 3, number 9, PB1 = 852Hz, PB2 = 1477Hz
+			row_delay = (TIMER / 852) / 2;
+			col_delay = (TIMER / 1477) / 2;
 			while(((PIND & (1 << PD4)) >> PD4) == 0);
 		}
 		else if (((PIND & (1 << PD5)) >> PD5) == 0) {
-			//column 4
-			//putchar('C');
-			//PB1 = 852Hz, PB2 = 1633Hz
-			row_delay = wave_852Hz();
-			col_delay = wave_1633Hz();
+			//column 4, letter C, PB1 = 852Hz, PB2 = 1633Hz
+			row_delay = (TIMER / 852) / 2;
+			col_delay = (TIMER / 1633) / 2;
 			while(((PIND & (1 << PD5)) >> PD5) == 0);
 		}
 		else {
@@ -142,35 +119,27 @@ void decode() {
 		PORTC &= ~(1 << PC5);
 		PORTC |= (1 << PC2) | (1 << PC3) | (1 << PC4);
 		if (((PIND & (1 << PD2)) >> PD2) == 0) {
-			//column 1;
-			//putchar('*');
-			//PB1 = 941Hz, PB2 = 1209Hz
-			row_delay = wave_941Hz();
-			col_delay = wave_1209Hz();
+			//column 1, letter *, PB1 = 941Hz, PB2 = 1209Hz
+			row_delay = (TIMER / 941) / 2;
+			col_delay = (TIMER / 1209) / 2;
 			while(((PIND & (1 << PD2)) >> PD2) == 0);
 		}
 		else if (((PIND & (1 << PD3)) >> PD3) == 0) {
-			//column 2
-			//putchar('0');
-			//PB1 = 941Hz, PB2 = 1336Hz
-			row_delay = wave_941Hz();
-			col_delay = wave_1336Hz();
+			//column 2, number 0, PB1 = 941Hz, PB2 = 1336Hz
+			row_delay = (TIMER / 941) / 2;
+			col_delay = (TIMER / 1336) / 2;
 			while(((PIND & (1 << PD3)) >> PD3) == 0);
 		}
 		else if (((PIND & (1 << PD4)) >> PD4) == 0) {
-			//column 3
-			//putchar('#');
-			//PB1 = 941Hz, PB2 = 1477Hz
-			row_delay = wave_941Hz();
-			col_delay = wave_1477Hz();
+			//column 3, letter #, PB1 = 941Hz, PB2 = 1477Hz
+			row_delay = (TIMER / 941) / 2;
+			col_delay = (TIMER / 1477) / 2;
 			while(((PIND & (1 << PD4)) >> PD4) == 0);
 		}
 		else if (((PIND & (1 << PD5)) >> PD5) == 0) {
-			//column 4
-			//putchar('D');
-			//PB1 = 941Hz, PB2 = 1633Hz
-			row_delay = wave_941Hz();
-			col_delay = wave_1633Hz();
+			//column 4, letter D, PB1 = 941Hz, PB2 = 1633Hz
+			row_delay = (TIMER / 941) / 2;
+			col_delay = (TIMER / 1633) / 2;
 			while(((PIND & (1 << PD5)) >> PD5) == 0);
 		}
 		else {
@@ -214,34 +183,6 @@ void toggle_off_tone() {
 	TIMSK1 &= ~(1 << OCIEB);   //disable OCB interrupt
 }
 
-int wave_697Hz() {
-	return ((TIMER / 697) / 2);
-}
-
-int wave_770Hz() {
-	return ((TIMER / 770) / 2);
-}
-
-int wave_852Hz() {
-	return ((TIMER / 852) / 2);
-}
-
-int wave_941Hz() {
-	return ((TIMER / 941) / 2);
-}
-
-int wave_1209Hz() {
-	return ((TIMER / 1209) / 2);
-}
-
-int wave_1336Hz() {
-	return ((TIMER / 1336) / 2);
-}
-
-int wave_1477Hz() {
-	return ((TIMER / 1477) / 2);
-}
-
 int wave_1633Hz() {
 	return ((TIMER / 1633) / 2);
 }
@@ -252,10 +193,8 @@ int main() {
 	DDRC |= (1 << PC2) | (1 << PC3) | (1 << PC4) | (1 << PC5);
 	//set columns as input
 	DDRD = 0;
-	
-	sei();
-
 	row_to_check = 4;
+	sei();
 
 	while(1);
 }
