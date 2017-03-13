87587e1a1a1bc75c1bae0f61b94481c781674e29
Victor Chek <VictorChek@hou0640.wireless-pennnet.upenn.edu>
2017-03-13 12:16:44 -0400

    changed while loop, added print statement to print ADC

--- Lab 5/L5_Part2.c
+++ Lab 5/L5_Part2.c
@@ -1,13 +1,16 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <stdio.h>
+#include "uart.h"
 
 
 int main() {
-	DDRC |= (1 << PC0);
-	ADMUX |= (1 << REFS0); //AVCC is ref voltage
-	ADCSRA |= (1 << ADEN) | (1 << ADATE);// enable ADC and auto trigger
-	ADCSRA |= (1 << ADSC); //start conversion
-	while(1) {
-		//PRINT ADC value from ADC reg
-	}
+	//Do we need to set analog channel?
+	//Do we need to change the conversion rate with ADCSRA?
+	DDRC   |= (1 << PC0);
+	ADMUX  |= (1 << REFS0);                //AVCC is ref voltage
+	ADCSRA |= (1 << ADEN) | (1 << ADATE);  //enable ADC and auto trigger
+	ADCSRA |= (1 << ADSC);                 //start conversion
+	while((ADCSRA & (1 << ADSC)) >> ADSC); //wait for conversion to finish
+	printf(ADC);
 }
