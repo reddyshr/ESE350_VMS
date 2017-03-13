b973f8708b5823c2aba877a47d702f5c62f48af1
Victor Chek <VictorChek@hou0640.wireless-pennnet.upenn.edu>
2017-03-13 12:42:41 -0400

    added analog channel statement so input is ADC0/PC0

--- Lab 5/L5_Part2.c
+++ Lab 5/L5_Part2.c
@@ -5,9 +5,9 @@
 
 
 int main() {
-	//Do we need to set analog channel?
 	//Do we need to change the conversion rate with ADCSRA?
 	DDRC   |= (1 << PC0);
+	ADMUX   = 0;                           //set analog channel as PC0/ADC0  
 	ADMUX  |= (1 << REFS0);                //AVCC is ref voltage
 	ADCSRA |= (1 << ADEN) | (1 << ADATE);  //enable ADC and auto trigger
 	ADCSRA |= (1 << ADSC);                 //start conversion
