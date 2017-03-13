cdea136737d27f37b49d848ef164af9c96051ffb
Victor Chek <VictorChek@levyvlan502.2882.wlan.wireless-resnet.upenn.edu>
2017-03-13 00:40:24 -0400

    switched placement of print statement, added code to reinitialize timer after printing out pulse width

--- Lab 5/L5_Part1.c
+++ Lab 5/L5_Part1.c
@@ -54,14 +54,13 @@ int main(void) {
 	
   while(1) {
 		if (sampleCollected) {
-			//PRINT DIFF
+			printf("Pulse width: %u\n", diff);
 			TIMSK1  = 0;             //disable interrupts
 			DDRB   |= (1 << PB1);    //PB1 is output
 			PORTB  |= (1 << PORTB1); //Set PB1 to high
 			TCNT1   = 0;             //clear timer count
-			TIMSK1 |= (1 << OCIE1A); //enable OCA interrupt
+			init_oc_timer1();        //reset so process repeats
 			sampleCollected = 0;
-			printf("Pulse width: %u\n", diff);
 		}
   }
 }
