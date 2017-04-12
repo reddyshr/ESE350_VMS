26d2fd56ab9fe3bf2dfba9aa9b4dc822bf8d8bbb
Victor Chek <VictorChek@levyvlan502.1177.wlan.wireless-resnet.upenn.edu>
2017-04-12 15:48:05 -0400

    Code for sensors rewritten, tested, need to convert clock cycles to distance

--- AutonomousCar/sensors.c
+++ AutonomousCar/sensors.c
@@ -51,6 +51,9 @@ volatile int delay;
 //PD3(Pin3) - SENSOR #5
 //PB1(Pin9) - TRIGGER PULSE
 
+int calculate_distance (int pulseWidth) {
+}
+
 void print_variables() {
 	printf("Sensor 1: start %u, end %u, pulse width %u\n", s1_start, s1_end, s1_pulse);
 	printf("Sensor 2: start %u, end %u, pulse width %u\n", s2_start, s2_end, s2_pulse);
@@ -60,12 +63,12 @@ void print_variables() {
 }
 
 void reset_variables() {
-	s1_ready = 0;
-	s2_ready = 0;
-	s3_ready = 0;
-	s4_ready = 0;
-	s5_ready = 0;
-	s1_start = 0;
+  s1_ready = 0;
+  s1_ready = 0;
+  s3_ready = 0;
+  s4_ready = 0;
+  s5_ready = 0;
+  s1_start = 0;
   s2_start = 0;
   s3_start = 0;
   s4_start = 0;
@@ -75,16 +78,16 @@ void reset_variables() {
   s3_end = 0;
   s4_end = 0;
   s5_end = 0;
-  s1_pulse = 0;
-  s2_pulse = 0;
-  s3_pulse = 0;
-  s4_pulse = 0;
-  s5_pulse = 0;
   s1_reading = 0;
   s2_reading = 0;
   s3_reading = 0;
   s4_reading = 0;
   s5_reading = 0;
+  s1_pulse = 0;
+  s2_pulse = 0;
+  s3_pulse = 0;
+  s4_pulse = 0;
+  s5_pulse = 0;
 }
 
 
@@ -107,7 +110,8 @@ void init_timer1() {
 	TCCR1A |= (1 << COM1A0);                //toggle OC1A(Pin9) on compare match
 	TIMSK1 |= (1 << OCIE1A);                //enable OCA interrupt
 	delay = triggerPulseHi;                 //start with high pulse
-	OCR1A = TCNT1 + delay;                  //2 cycles = 10us needed
+	TCNT1 = 0;
+  OCR1A = TCNT1 + delay;                  //2 cycles = 10us needed
 }
 
 int main() {
@@ -125,72 +129,69 @@ int main() {
 	init_timer1();
 
   while(1) {
-  	//check for input from sensors continously
-  	/*if ((s1_ready == 1) && (s2_ready == 1) && (s3_ready == 1) && (s4_ready == 1) && (s5_ready == 1)) {
-  		//sensor readings are complete
-  		print_variables();
-  		//reset_variables();
-  		break;
-  	}
-  	else if ((((PIND & (1 << PIND7)) >> PIND7) == 1) && s1_reading == 0  && s1_ready == 0) {
-  		//sensor 1 rising edge
-  		s1_start = TCNT1;
-  		s1_reading = 1;
-  	}
-  	else if ((((PIND & (1 << PIND7)) >> PIND7) == 0) && s1_reading == 1 && s1_ready == 0) {
-  		//sensor 1 falling edge
-  		s1_end = TCNT1;
-  		s1_pulse = s1_end - s1_start;
-  		s1_ready = 1;
-  	}
-  	else if ((((PIND & (1 << PIND6)) >> PIND6) == 1) && s2_reading == 0  && s2_ready == 0) {
-  		//sensor 2 rising edge
-  		s2_start = TCNT1;
-  		s2_reading = 1;
-  	}
-  	else if ((((PIND & (1 << PIND6)) >> PIND6) == 0) && s2_reading == 1  && s2_ready == 0) {
-  		//sensor 2 falling edge
-  		s2_end = TCNT1;
-  		s2_pulse = s2_end - s2_start;
-  		s2_ready = 1;
-  	}
-  	else if ((((PIND & (1 << PIND5)) >> PIND5) == 1) && s2_reading == 0  && s2_ready == 0) {
-  		//sensor 3 rising edge
-  		s3_start = TCNT1;
-  		s3_reading = 1;
-  	}
-  	else if ((((PIND & (1 << PIND5)) >> PIND5) == 0) && s2_reading == 1  && s2_ready == 0) {
-  		//sensor 3 falling edge
-  		s3_end = TCNT1;
-  		s3_pulse = s3_end - s3_start;
-  		s3_ready = 1;
-  	}
-  	else if ((((PIND & (1 << PIND4)) >> PIND4) == 1) && s2_reading == 0  && s2_ready == 0) {
-  		//sensor 4 rising edge
-  		s4_start = TCNT1;
-  		s4_reading = 1;
-  	}
-  	else if ((((PIND & (1 << PIND4)) >> PIND4) == 0) && s2_reading == 1  && s2_ready == 0) {
-  		//sensor 4 falling edge
-  		s4_end = TCNT1;
-  		s4_pulse = s4_end - s4_start;
-  		s4_ready = 1;
-  	}
-  	else if ((((PIND & (1 << PIND3)) >> PIND3) == 1) && s2_reading == 0  && s2_ready == 0) {
-  		//sensor 5 rising edge
-  		s5_start = TCNT1;
-  		s5_reading = 1;
-  	}
-  	else if ((((PIND & (1 << PIND3)) >> PIND3) == 0) && s2_reading == 1  && s2_ready == 0) {
-  		//sensor 5 falling edge
-  		s5_end = TCNT1;
-  		s5_pulse = s5_end - s5_start;
-  		s5_ready = 1;
-  	}
-  	else {
-  		printf("reading...\n");
-  	}*/
+  	if (delay == triggerPulseLo) {
+      //check for input from sensors continously
+      if (s1_ready == 1 && s2_ready == 1 && s3_ready == 1) {
+        print_variables();
+        break;
+      }
+      if ((((PIND & (1 << PIND7)) >> PIND7) == 1) && s1_reading == 0 && s1_ready == 0) {
+      //sensor 1 rising edge
+        s1_start = TCNT1;
+        s1_reading = 1;
+      }
+      if ((((PIND & (1 << PIND7)) >> PIND7) == 0) && s1_reading == 1 && s1_ready == 0) {
+      //sensor 1 falling edge
+        s1_end = TCNT1;
+        s1_pulse = s1_end - s1_start;
+        s1_ready = 1;
+      }  
+      if ((((PIND & (1 << PIND6)) >> PIND6) == 1) && s2_reading == 0 && s2_ready == 0) {
+      //sensor 2 rising edge
+        s2_start = TCNT1;
+        s2_reading = 1;
+      }
+      if ((((PIND & (1 << PIND6)) >> PIND6) == 0) && s2_reading == 1 && s2_ready == 0) {
+      //sensor 2 falling edge
+        s2_end = TCNT1;
+        s2_pulse = s2_end - s2_start;
+        s2_ready = 1;
+      }
+      if ((((PIND & (1 << PIND5)) >> PIND5) == 1) && s3_reading == 0 && s3_ready == 0) {
+      //sensor 3 rising edge
+        s3_start = TCNT1;
+        s3_reading = 1;
+      }
+      if ((((PIND & (1 << PIND5)) >> PIND5) == 0) && s3_reading == 1 && s3_ready == 0) {
+      //sensor 3 falling edge
+        s3_end = TCNT1;
+        s3_pulse = s3_end - s3_start;
+        s3_ready = 1;
+      }
+      if ((((PIND & (1 << PIND4)) >> PIND4) == 1) && s4_reading == 0 && s4_ready == 0) {
+      //sensor 4 rising edge
+        s4_start = TCNT1;
+        s4_reading = 1;
+      }
+      if ((((PIND & (1 << PIND4)) >> PIND4) == 0) && s4_reading == 1 && s4_ready == 0) {
+      //sensor 4 falling edge
+        s4_end = TCNT1;
+        s4_pulse = s4_end - s4_start;
+        s4_ready = 1;
+      }
+      if ((((PIND & (1 << PIND3)) >> PIND3) == 1) && s5_reading == 0 && s5_ready == 0) {
+      //sensor 5 rising edge
+        s5_start = TCNT1;
+        s5_reading = 1;
+      }
+      if ((((PIND & (1 << PIND3)) >> PIND3) == 0) && s5_reading == 1 && s5_ready == 0) {
+      //sensor 5 falling edge
+        s5_end = TCNT1;
+        s5_pulse = s5_end - s5_start;
+        s5_ready = 1;
+      }
+    }
+    
   }
-
 	return 1;
 }
