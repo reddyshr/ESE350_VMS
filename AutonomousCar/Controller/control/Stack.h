f232171e7dcf374d7405b1266186f2c9ace0177f
Shreetej Reddy <reddyshr@sas.upenn.edu>
2017-04-23 23:43:28 -0400

    basic stack implementation

---
+++ AutonomousCar/Controller/control/Stack.h
@@ -0,0 +1,36 @@
+/*
+ * Stack.h
+ *
+ * Created: 4/23/2017 10:09:42 PM
+ *  Author: Shreetej
+ */ 
+
+
+#ifndef STACK_H_
+#define STACK_H_
+
+class Stack {
+
+  struct Node {
+    int decision;
+  };
+  
+  public:
+    Stack();
+    void pushVal(int val);
+    int popVal();
+    
+  private:
+      int head;
+      int arr[20];
+  
+  
+  
+    
+};
+
+
+
+#endif /* STACK_H_ */
+
+
